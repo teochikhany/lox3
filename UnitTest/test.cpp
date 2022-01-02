c50e7b65c2daa7b8216a66abf044ce264505e5c2
teo chikhany <teochikhany@icloud.com>
2022-01-02 09:32:45 +0200

    arithmatic unit test

--- UnitTest/test.cpp
+++ UnitTest/test.cpp
@@ -31,9 +31,9 @@ void Helper::Walk(antlr4::tree::ParseTreeListener* t, antlr4::tree::ParseTree* t
     tree::ParseTreeWalker::DEFAULT.walk(t, t2);
 }
 
-TEST(arithmatique, addition) {
-
-    antlr4::ANTLRInputStream input("2+5;");
+Value custom_main(std::string string_input)
+{
+    antlr4::ANTLRInputStream input(string_input);
     loxLexer lexer(&input);
     antlr4::CommonTokenStream tokens(&lexer);
     loxParser parser(&tokens);
@@ -50,10 +50,91 @@ TEST(arithmatique, addition) {
 
     Value lastElement = stack.at(stack.size() - 1);
 
+    delete vm;
+    delete chunk;
+
+    return lastElement;
+}
+
+
+TEST(arithmatique, addition) {
+
+    Value lastElement = custom_main("2+5;");
+
     EXPECT_TRUE(lastElement.isNumber() );
     EXPECT_EQ(7.0, lastElement.getDouble() );
+}
 
-    delete vm;
+
+TEST(arithmatique, addition_negative) {
+
+    Value lastElement = custom_main("2 + (-5);");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(-3.0, lastElement.getDouble());
+}
+
+
+TEST(arithmatique, addition_negative_2) {
+
+    Value lastElement = custom_main("2 + -5;");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(-3.0, lastElement.getDouble());
+}
+
+
+TEST(arithmatique, multiplication) {
+
+    Value lastElement = custom_main("3 * 4;");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(12.0, lastElement.getDouble());
+}
+
+
+TEST(arithmatique, multiplication_negative) {
+
+    Value lastElement = custom_main("3 * -4;");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(-12.0, lastElement.getDouble());
+}
+
+
+TEST(arithmatique, substraction) {
+
+    Value lastElement = custom_main("4 - 1;");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(3.0, lastElement.getDouble());
+}
+
+
+TEST(arithmatique, substraction_negative) {
+
+    Value lastElement = custom_main("3 - 4;");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(-1.0, lastElement.getDouble());
+}
+
+
+TEST(arithmatique, order_precendence) {
+
+    Value lastElement = custom_main("3 + 6 * 4;");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(27.0, lastElement.getDouble());
+}
+
+
+TEST(arithmatique, order_precendence_2) {
+
+    Value lastElement = custom_main("(3 + 6) * 4;");
+
+    EXPECT_TRUE(lastElement.isNumber());
+    EXPECT_EQ(36.0, lastElement.getDouble());
 }
 
 
