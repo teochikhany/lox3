fe36d6bd2981506bf8c304c457621cd705206409
teo chikhany <teochikhany@icloud.com>
2022-01-03 10:16:08 +0200

    more variables unit test

--- UnitTest/variables.cpp
+++ UnitTest/variables.cpp
@@ -1,7 +1,7 @@
 #include "Utils.h"
 
 
-TEST(global_var, bool_true) {
+TEST(global_var_define, bool_true) {
 
     Value variable = custom_main_first_global("var x = true;", "x");
     EXPECT_TRUE(variable.isBool());
@@ -9,7 +9,7 @@ TEST(global_var, bool_true) {
 }
 
 
-TEST(global_var, bool_false) {
+TEST(global_var_define, bool_false) {
 
     Value variable = custom_main_first_global("var x = false;", "x");
     EXPECT_TRUE(variable.isBool());
@@ -17,14 +17,21 @@ TEST(global_var, bool_false) {
 }
 
 
-TEST(global_var, nil) {
+TEST(global_var_define, nil) {
 
     Value variable = custom_main_first_global("var x = nil;", "x");
     EXPECT_TRUE(variable.isNil());
 }
 
 
-TEST(global_var, number) {
+TEST(global_var_define, nil_2) {
+
+    Value variable = custom_main_first_global("var x;", "x");
+    EXPECT_TRUE(variable.isNil());
+}
+
+
+TEST(global_var_define, number) {
 
     Value variable = custom_main_first_global("var x = 2.5;", "x");
     EXPECT_TRUE(variable.isNumber());
@@ -32,9 +39,58 @@ TEST(global_var, number) {
 }
 
 
-TEST(global_var, string) {
+TEST(global_var_define, string) {
 
     Value variable = custom_main_first_global("var x = \"variable\";", "x");
     EXPECT_TRUE(variable.isStr());
     EXPECT_EQ("\"variable\"", variable.getString());
+}
+
+
+////////////////
+
+
+TEST(global_var_get, bool_true) {
+
+    Value topStack = custom_main_top_stack("var x = true; x;");
+    EXPECT_TRUE(topStack.isBool());
+    EXPECT_EQ(true, topStack.getBool());
+}
+
+
+TEST(global_var_get, bool_false) {
+
+    Value topStack = custom_main_top_stack("var x = false; x;");
+    EXPECT_TRUE(topStack.isBool());
+    EXPECT_EQ(false, topStack.getBool());
+}
+
+
+TEST(global_var_get, nil) {
+
+    Value topStack = custom_main_top_stack("var x = nil; x;");
+    EXPECT_TRUE(topStack.isNil());
+}
+
+
+TEST(global_var_get, nil_2) {
+
+    Value topStack = custom_main_top_stack("var x; x;");
+    EXPECT_TRUE(topStack.isNil());
+}
+
+
+TEST(global_var_get, number) {
+
+    Value topStack = custom_main_top_stack("var x = 2.5; x;");
+    EXPECT_TRUE(topStack.isNumber());
+    EXPECT_EQ(2.5, topStack.getDouble());
+}
+
+
+TEST(global_var_get, string) {
+
+    Value topStack = custom_main_top_stack("var x = \"variable\"; x;");
+    EXPECT_TRUE(topStack.isStr());
+    EXPECT_EQ("\"variable\"", topStack.getString());
 }
